43ade8379eceaef7528facf083f4529a142e41e2
rickszyr <rickszyr@users.noreply.github.com>
2016-04-29 10:17:53 -0300

    Added Project with Tests
    Contains basic tests with commented evolution of the class code.

---
+++ Entities/PalindromeChecker.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class PalindromeChecker
+    {
+        //public bool Check(string testString)
+        //{
+        //    return true;
+        //}
+
+        //public bool Check(string testString)
+        //{
+        //    if (testString.Length < 2)
+        //        return true;
+        //    return testString[0] == testString[1];
+        //}
+
+        public bool Check(string testString)
+        {
+            for (int i = 0, j = testString.Length - 1; i < j; i++, j--)
+            {
+                if (testString[i] != testString[j])
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Tests/PalindromeCheckerTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Entities;
+
+namespace Tests
+{
+    [TestClass]
+    public class PalindromeCheckerTests
+    {
+        [TestMethod]
+        public void CheckEmptyStringIsPalindromeTest()
+        {
+            PalindromeChecker checker = new PalindromeChecker();
+
+            Assert.IsTrue(checker.Check(""));
+        }
+
+        [TestMethod]
+        public void CheckStringLengthOneIsPalindromeTest()
+        {
+            PalindromeChecker checker = new PalindromeChecker();
+
+            Assert.IsTrue(checker.Check("A"));
+        }
+
+        [TestMethod]
+        public void CheckStringLengthTwoIsPalindromeTest()
+        {
+            PalindromeChecker checker = new PalindromeChecker();
+
+            Assert.IsTrue(checker.Check("AA"));
+        }
+
+        [TestMethod]
+        public void CheckStringLengthTwoIsNotPalindromeTest()
+        {
+            PalindromeChecker checker = new PalindromeChecker();
+
+            Assert.IsFalse(checker.Check("AB"));
+        }
+
+        [TestMethod]
+        public void CheckStringLength2NIsPalindromeTest()
+        {
+            PalindromeChecker checker = new PalindromeChecker();
+
+            Assert.IsTrue(checker.Check("ABBA"));
+        }
+
+        [TestMethod]
+        public void CheckStringLength2NIsNotPalindromeTest()
+        {
+            PalindromeChecker checker = new PalindromeChecker();
+
+            Assert.IsFalse(checker.Check("ABBC"));
+        }
+
+        [TestMethod]
+        public void CheckStringLength2N1IsPalindromeTest()
+        {
+            PalindromeChecker checker = new PalindromeChecker();
+
+            Assert.IsTrue(checker.Check("ABCBA"));
+        }
+
+        [TestMethod]
+        public void CheckStringLength2N1IsNotPalindromeTest()
+        {
+            PalindromeChecker checker = new PalindromeChecker();
+
+            Assert.IsFalse(checker.Check("ABCBC"));
+        }
+    }
+}
